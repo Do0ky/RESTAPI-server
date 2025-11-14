05cbf344838e725aaeb286624df5174e798fd030
Dooky <chamallow14@hotmail.fr>
2025-11-14 18:10:15 -0300

    Workshop 4 task 3

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const campsiteRouter = require ('./routes/campsiteRouter');
 const promotionRouter = require ('./routes/promotionRouter');
 const partnerRouter = require ('./routes/partnerRouter');
 const uploadRouter = require ('./routes/uploadRouter');
+const favoriteRouter = require ('./routes/favoriteRouter');
 
 //Mongoose wrapper methods around the mongoDB node driver
 const mongoose = require('mongoose');
@@ -52,6 +53,7 @@ app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
